506b89fb8436a3e1e108817a9ff4d752e0d8656e
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-04-03 16:30:18 +0300

    step5

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,6 +4,8 @@ export default function Feedback({ reviews: { good, neutral, bad }, total }) {
       {total > 0 && <p> Good: {good}</p>}
       {total > 0 && <p>Neutral: {neutral}</p>}
       {total > 0 && <p>Bad: {bad}</p>}
+      {total > 0 && <p>Total: {total}</p>}
+      {total > 0 && <p>Positive: {Math.round((good / total) * 100)}%</p>}
     </>
   );
 }
